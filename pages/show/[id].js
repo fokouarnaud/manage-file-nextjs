e3c768ed3b3217224976f5e1de81779414edeed3
fokouarnaud <fokouarnaud@gmail.com>
2022-06-30 16:58:51 +0100

    Update [id].js

--- pages/show/[id].js
+++ pages/show/[id].js
@@ -22,7 +22,7 @@ const DetailPage = () => {
 
     <section className="mb-6 p-6 max-w-5xl  text-gray-600 body-font container px-5 py-24 mx-auto">
       <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-        <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+        <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-gray-500 flex-shrink-0">
           <svg xmlns="http://www.w3.org/2000/svg" className="sm:w-16 sm:h-16 w-10 h-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
           </svg>
