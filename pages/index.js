8eca97454ff9bcdea0a3aa2b1a766686e443eb8a
fokouarnaud <fokouarnaud@gmail.com>
2022-07-06 17:35:49 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head';
 import FilterSelect from '../components/filterSelect';
 import MyListBox from '../components/MyListBox';
+import { useState } from 'react';
 
 const Home = () => {
+  const [myfilter, setMyFilter] = useState({
+    departement: "",
+    type_doc: "",
+    annee: ""
+  });
 
   const items_departement = [
     {
@@ -89,15 +95,37 @@ const Home = () => {
           <h1 className=" md:w-1/4 text-4xl font-extrabold tracking-tight text-gray-900">Documents</h1>
           <div className=' p-6 pb-6 border-4 border-dashed border-gray-200 rounded-lg md:w-3/4 flex flex-col md:flex-row md:flex-wrap gap-y-3 '>
             <div className='flex flex-col mt-6 md:mt-0 mr-6'>
-              <FilterSelect key={1} label={"Departement"} items={items_departement} />
+              <label className="block">
+                <span className="text-gray-700">Departement</span>
+                <select className=" cursor-pointer block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0
+                  ">{items_departement.map(item => (
+                  <option key={item.id} value={`${item.name}`}>{item.name}</option>
+                ))}
+                </select>
+              </label>
             </div>
-            <div className='flex flex-col mt-0 md:mt-0 mr-6 '>
-              <FilterSelect key={2} label={"Type document"} items={items_type_doc} />
+            <div className='flex flex-col mt-6 md:mt-0 mr-6'>
+              <label className="block">
+                <span className="text-gray-700">Type document</span>
+                <select className=" cursor-pointer block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0
+                  ">{items_type_doc.map(item => (
+                  <option key={item.id} value={`${item.name}`}>{item.name}</option>
+                ))}
+                </select>
+              </label>
             </div>
-            <div className='flex flex-col mt-0 md:mt-0 mr-6'>
-              <FilterSelect key={3} label={"Annee"} items={items_annee} />
+            <div className='flex flex-col mt-6 md:mt-0 mr-6'>
+              <label className="block">
+                <span className="text-gray-700">Annee</span>
+                <select className=" cursor-pointer block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0
+                  ">{items_annee.map(item => (
+                  <option key={item.id} value={`${item.name}`}>{item.name}</option>
+                ))}
+                </select>
+              </label>
             </div>
-            <button type="button" className=" inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out">Filtrer</button>
+            
+            <button type="button" className=" mt-6 inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out">Filtrer</button>
           </div>
 
 
