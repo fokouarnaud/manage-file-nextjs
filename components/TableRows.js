8eaa8a15054abc15ae0ce1a831602e45e3e21548
fokouarnaud <fokouarnaud@gmail.com>
2022-07-06 09:32:59 +0100

    Update TableRows.js

--- components/TableRows.js
+++ components/TableRows.js
@@ -26,8 +26,8 @@ const TableRows = ({
                 {departementEtudiant}
             </td>
             <td className="px-4 py-3 text-sm">
-            <Link href={`/show/${id}`}>
-               <a className='font-medium leading-5 text-purple-600 underline underline-offset-2' hreh="#">{titreDoc}</a> 
+                <Link href={`/show/${id}`}>
+                    <a className='font-medium leading-5 text-purple-600 underline underline-offset-4' hreh="#">{titreDoc}</a>
                 </Link>
 
             </td>
@@ -36,7 +36,7 @@ const TableRows = ({
             </td>
             <td className="px-4 py-3">
                 <div className="flex items-center space-x-4 text-sm">
-                    
+
                     <Link href={`/show/${id}`}>
                         <button
                             className="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg  focus:outline-none focus:shadow-outline-gray"
@@ -44,8 +44,7 @@ const TableRows = ({
                         >
 
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                             </svg>
 
 
